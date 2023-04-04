0aa6cf4f5fa50ef2040d7b530fbd03c54fec9e8c
emamingio22 <emawankenoby02@gmail.com>
2023-04-04 16:49:49 +0200

    i made the while

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-const listsSpesa = ["Acqua", "Pane", "Birra", "Lievito", "Mele", "Farina", "Uova"];
+const listaSpesa = ["Acqua", "Pane", "Birra", "Lievito", "Mele", "Farina", "Uova"];
 const shoppingList = document.getElementById("shopping-list");
-console.log(listsSpesa)
+console.log(listaSpesa)
 
+
+let i = 0;
+
+
+while (i < listaSpesa.length) {
+    let item = `<li>${listaSpesa[i]}</li>`
+    shoppingList.innerHTML += item;
+    i++;  
+}
